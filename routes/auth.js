2fc10067e68aa84f41b5f58ad34fe73c21037343
Grey22 <klsammie24@gmail.com>
2021-03-17 09:40:23 +0100

    ok

--- routes/auth.js
+++ routes/auth.js
@@ -1,33 +1,37 @@
-  
-const express = require("express")
+const express = require("express");
 
+const {
+  getUsers,
+  updateAccount,
+  getSingleUser,
+  login,
+  register,
+  getMe,
+  uploadUserPhoto,
+  logout,
+  updateUserDetails,
+  resetPassword,
+  forgotPassword,
+  deleteUser,
+} = require("../controller/auth");
+const { protect, authorize } = require("../middleWare/auth");
 
-const { getUsers,updateAccount,getSingleUser,login,register,getMe,uploadUserPhoto,logout,updateUserDetails,resetPassword,forgotPassword, deleteUser} = require("../controller/auth")
-const { protect,authorize } = require("../middleWare/auth")
+const Router = express.Router();
 
+//Route for photo upload
+Router.route("/:id/photo").put(protect, uploadUserPhoto);
+console.log("req.headers.authorization");
+Router.route("/register").post(register);
+Router.route("/login").post(login);
+Router.route("/forgot-password").post(forgotPassword);
+Router.get("/logout", protect, logout);
+Router.get("/get-users", protect, getUsers);
+Router.get("/get-user/:id", protect, getSingleUser);
+Router.get("/getMe/:id", protect, getMe);
+Router.put("/update-account/:id", protect, updateAccount);
+Router.put("/update/:id", protect, authorize("admin"), updateUserDetails);
+Router.delete("/delete/:id", protect, authorize("admin"), deleteUser);
+Router.put("/reset-password/:id", resetPassword);
+Router.put("/photo/:id", protect, uploadUserPhoto);
 
-const Router = express.Router()
-
-
-
- //Route for photo upload
-Router.route("/:id/photo").put(
-    protect,
-    uploadUserPhoto
-  );
-  console.log("req.headers.authorization")
-Router.route("/register").post(register)   
-Router.route("/login").post(login)
-Router.route("/forgot-password").post(forgotPassword) 
-Router.get("/logout",protect,logout)
-Router.get("/get-users",protect,getUsers) 
-Router.get("/get-user/:id",protect,getSingleUser) 
-Router.get("/getMe/:id",protect,getMe) 
-Router.put("/update-account/:id",protect,updateAccount)
-Router.put("/update/:id",protect,authorize('admin'),updateUserDetails)
-Router.delete("/delete/:id",protect,authorize('admin'),deleteUser)
-Router.put("/reset-password/:id",protect, resetPassword ) 
-Router.put("/photo/:id",protect,uploadUserPhoto ) 
- 
-
-module.exports= Router   
+module.exports = Router;
